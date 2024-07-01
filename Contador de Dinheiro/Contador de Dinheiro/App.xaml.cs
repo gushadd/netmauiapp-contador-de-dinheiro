d8f245a6ab669211d55911314dfa4a6273a994dd
Gustavo <gustavoeliashaddad@gmail.com>
2024-07-01 17:45:13 -0300

    ui: selecao das cores

--- Contador de Dinheiro/Contador de Dinheiro/App.xaml.cs
+++ Contador de Dinheiro/Contador de Dinheiro/App.xaml.cs
@@ -1,10 +1,46 @@
-﻿namespace Contador_de_Dinheiro
+﻿#if ANDROID
+
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+            
+#endif
+
+namespace Contador_de_Dinheiro
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("MyCustomization", (handler, view) =>
+            {
+#if ANDROID
+                handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+
+
+#endif
+            });
+
+
+            Microsoft.Maui.Handlers.EditorHandler.Mapper.AppendToMapping("MyCustomization", (handler, view) =>
+            {
+#if ANDROID
+                handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+
+
+#endif
+            });
+
+
+            Microsoft.Maui.Handlers.PickerHandler.Mapper.AppendToMapping("MyCustomization", (handler, view) =>
+            {
+#if ANDROID
+                handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+
+
+#endif
+            });
+
 
             MainPage = new AppShell();
         }
