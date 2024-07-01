133586cce9688d8264d2fa8d5a0ad81d09f628af
Gustavo <gustavoeliashaddad@gmail.com>
2024-07-01 13:25:20 -0300

    ui: adicao das fontes

--- Contador de Dinheiro/Contador de Dinheiro/MauiProgram.cs
+++ Contador de Dinheiro/Contador de Dinheiro/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Hosting;
 
 namespace Contador_de_Dinheiro
 {
@@ -13,6 +14,16 @@ namespace Contador_de_Dinheiro
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+                    fonts.AddFont("Inter-Black.ttf", "InterBlack");
+                    fonts.AddFont("Inter-Bold.ttf", "InterBold");
+                    fonts.AddFont("Inter-ExtraBold.ttf", "InterExtraBold");
+                    fonts.AddFont("Inter-ExtraLight.ttf", "InterExtraLight");
+                    fonts.AddFont("Inter-Light.ttf", "InterLight");
+                    fonts.AddFont("Inter-Medium.ttf", "InterMedium");
+                    fonts.AddFont("Inter-Regular.ttf", "InterRegular");
+                    fonts.AddFont("Inter-SemiBold.ttf", "InterSemiBold");
+                    fonts.AddFont("Inter-Thin.ttf", "InterThin");                    
                 });
 
 #if DEBUG
